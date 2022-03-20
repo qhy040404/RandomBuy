bbe8fc6e2613377ea0dbcb9b920211533fa17964
qhy040404 <qhy040404@163.com>
2022-03-20 11:44:30 +0800

    update

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ A little program for myself to record my saving experience.
 - The code is all written in Visual Studio 2022 Community. 
 - Download Visual Studio 2022 Community for free and open ```.sln``` the solution file. 
 - And then build the code.
+
+## Notice
+
+- The program is using Chinese, if you want to use other language, translate it by yourself or Google.
+- 瞎jb写的，能用就行
+
+- 嗨害嗨
--- RandomBuy/源.cpp
+++ RandomBuy/源.cpp
@@ -5,6 +5,7 @@
 #include <windows.h>
 #include <string>
 #include <fstream>
+#include <conio.h>
 using namespace std;
 int main()
 {
@@ -19,16 +20,35 @@ int main()
 	double input;
 	double random;
 	double sum = 0;
-	double withdraw;
+	double withdraw = 0;
 	cout << "今天搞了多少啊：";
 	cin >> input;
-	srand((unsigned)time(NULL));
-	random = double(rand() % 100) / 100;
-	withdraw = input * random;
-	__int64 temp = (__int64)(withdraw * 100);
-	withdraw = temp / 100.0;
-	cout << setiosflags(ios::fixed) << setprecision(2);
-	cout << "存这么多吧：" << withdraw;
+	while (1)
+	{
+		cout << endl << "全存（1）还是怎么的（2）：";
+		char ch;
+		ch = _getch();
+		if (ch == 49)
+		{
+			withdraw = input;
+			break;
+		}
+		else if (ch == 50)
+		{
+			srand((unsigned)time(NULL));
+			random = double(rand() % 100) / 100;
+			withdraw = input * random;
+			__int64 temp = (__int64)(withdraw * 100);
+			withdraw = temp / 100.0;
+			cout << setiosflags(ios::fixed) << setprecision(2);
+			cout << endl << "存这么多吧：" << withdraw;
+			break;
+		}
+		else
+		{
+			cout << endl << "咋瞎jb乱输呢" << endl;
+		}
+	}
 	fstream save;
 	save.open(TxtPath.c_str());
 	save >> sum;
